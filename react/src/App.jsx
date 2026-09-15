19821b0b2dfe4c1bfe5b50579c7a0c0e32c278a6
Your Name <your.email@example.com>
2026-09-15 21:59:59 +0500

    fix components and spacing

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,8 @@
-import Cards from "../components/Cards";
-import Carousel from "../components/Carousel";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Cards from "./components/Cards";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 function App() {
     return (
         <>
--- react/src/components/Cards.jsx
+++ react/src/components/Cards.jsx
@@ -78,4 +78,4 @@ const Cards = () => {
 
 }
 
-export default Cardscards.jsx
+export default Cards
